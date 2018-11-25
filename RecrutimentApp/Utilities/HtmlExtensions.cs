7c6e5804cf1187c230b94e49728afb8e63c9e9af
Sebastian Safin <sebastian.safin@outlook.com>
2018-11-25 15:55:20 +0000

    Merged PR 8: Refactor of cshtml files with HtmlExtensions
    Refactor of cshtml files with HtmlExtensions
    
    Related work items: #13

--- RecrutimentApp/Utilities/HtmlExtensions.cs
+++ RecrutimentApp/Utilities/HtmlExtensions.cs
@@ -9,9 +9,64 @@ namespace RecrutimentApp
     {
         public static IHtmlContent LabelForWithEmptyLine<TModel, TResult>(this IHtmlHelper<TModel> htmlHelper, Expression<Func<TModel, TResult>> expression)
         {
-            HtmlContentBuilder contentBuilder = new HtmlContentBuilder();
+            HtmlContentBuilder contentBuilder = new HtmlContentBuilder(2);
             contentBuilder.AppendHtml(htmlHelper.LabelFor(expression));
             contentBuilder.AppendHtml(new HtmlString("<br/>"));
+            return contentBuilder;
+        }
+
+        public static IHtmlContent LabelEmptyLineDisplayFor<TModel, TResult>(this IHtmlHelper<TModel> htmlHelper, Expression<Func<TModel, TResult>> expression)
+        {
+            HtmlContentBuilder contentBuilder = new HtmlContentBuilder(2);
+            contentBuilder.AppendHtml(htmlHelper.LabelForWithEmptyLine(expression));
+            contentBuilder.AppendHtml(htmlHelper.DisplayFor(expression, new { htmlAttributes = new { @class = "form-control" } }));
+            return contentBuilder;
+        }
+
+        public static IHtmlContent LabelEditorValidationMessageFor<TModel, TResult>(this IHtmlHelper<TModel> htmlHelper, Expression<Func<TModel, TResult>> expression)
+        {
+            HtmlContentBuilder contentBuilder = new HtmlContentBuilder(3);
+            contentBuilder.AppendHtml(htmlHelper.LabelFor(expression));
+            contentBuilder.AppendHtml(htmlHelper.EditorFor(expression, new { htmlAttributes = new { @class = "form-control" } }));
+            contentBuilder.AppendHtml(htmlHelper.ValidationMessageFor(expression, "", new { @class = "text-danger" }));
+            return contentBuilder;
+        }
+
+        public static IHtmlContent LabelTextAreaValidationMessageFor<TModel, TResult>(this IHtmlHelper<TModel> htmlHelper,
+            Expression<Func<TModel, TResult>> expression, string placeholder = "")
+        {
+            HtmlContentBuilder contentBuilder = new HtmlContentBuilder(3);
+            contentBuilder.AppendHtml(htmlHelper.LabelFor(expression));
+            contentBuilder.AppendHtml(htmlHelper.TextAreaFor(expression, new { @class = "form-control", placeholder }));
+            contentBuilder.AppendHtml(htmlHelper.ValidationMessageFor(expression, "", new { @class = "text-danger" }));
+            return contentBuilder;
+        }
+
+        public static IHtmlContent LabelCalendarEditorValidationMessageFor<TModel, TResult>(this IHtmlHelper<TModel> htmlHelper, Expression<Func<TModel, TResult>> expression)
+        {
+            HtmlContentBuilder contentBuilder = new HtmlContentBuilder(8);
+            contentBuilder.AppendHtml(htmlHelper.LabelFor(expression));
+            contentBuilder.AppendHtml(new HtmlString("<div class=\"input-group\">"));
+            contentBuilder.AppendHtml(htmlHelper.EditorFor(expression, new { htmlAttributes = new { @class = "form-control" } }));
+            contentBuilder.AppendHtml(new HtmlString("<span class=\"input-group-addon\">"));
+            contentBuilder.AppendHtml(new HtmlString("<span class=\"glyphicon glyphicon-calendar\"></span>"));
+            contentBuilder.AppendHtml(new HtmlString("</span>"));
+            contentBuilder.AppendHtml(new HtmlString("</div>"));
+            contentBuilder.AppendHtml(htmlHelper.ValidationMessageFor(expression, "", new { @class = "text-danger" }));
+            return contentBuilder;
+        }
+
+        public static IHtmlContent LabelMoneyEditorValidationMessageFor<TModel, TResult>(this IHtmlHelper<TModel> htmlHelper,
+            Expression<Func<TModel, TResult>> expression, string placeholder = "")
+        {
+            HtmlContentBuilder contentBuilder = new HtmlContentBuilder(7);
+            contentBuilder.AppendHtml(htmlHelper.LabelFor(expression));
+            contentBuilder.AppendHtml(new HtmlString("<div class=\"input-group\">"));
+            contentBuilder.AppendHtml(new HtmlString("<span class=\"input-group-addon\">PLN</span>"));
+            contentBuilder.AppendHtml(htmlHelper.EditorFor(expression, new { htmlAttributes = new { @class = "form-control", placeholder } }));
+            contentBuilder.AppendHtml(new HtmlString("<span class=\"input-group-addon\">.00</span>"));
+            contentBuilder.AppendHtml(new HtmlString("</div>"));
+            contentBuilder.AppendHtml(htmlHelper.ValidationMessageFor(expression, "", new { @class = "text-danger" }));
             return contentBuilder;
         }
     }
