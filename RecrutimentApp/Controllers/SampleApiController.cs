597ce485a3b8d30030069190eda1b7180a65f1bb
Sebastian Safin <sebastian.safin@outlook.com>
2018-12-26 18:54:40 +0000

    Merged PR 13: Added swagger library, commented SampleApi
    Added swagger library, commented SampleApi
    
    Related work items: #22, #23

--- RecrutimentApp/Controllers/SampleApiController.cs
+++ RecrutimentApp/Controllers/SampleApiController.cs
@@ -9,6 +9,7 @@ using RecrutimentApp.Models;
 
 namespace RecrutimentApp.Controllers
 {
+    [ApiController]
     [Produces("application/json")]
     [Route("api/[controller]/[action]")]
     public class SampleApiController : Controller
@@ -20,6 +21,14 @@ namespace RecrutimentApp.Controllers
             this.dataContext = dataContext ?? throw new ArgumentNullException(nameof(dataContext));
         }
 
+        /// <summary>
+        /// Gets job offers which title contains given string
+        /// </summary>
+        /// <remarks>
+        /// If searchString is null or empty, all job offers are returned. 
+        /// </remarks>
+        /// <param name="searchString">Search string which will be used to filter job offers</param>
+        /// <returns>Job offers which title contains given string</returns>
         [HttpGet]
         [HttpGet("{searchString}")]
         public async Task<IEnumerable<JobOffer>> GetJobOffers(string searchString)
@@ -40,12 +49,28 @@ namespace RecrutimentApp.Controllers
                 (o, c) => new JobOffer(o, c)).ToListAsync();
         }
 
+        /// <summary>
+        /// Gets job applications which are associated with job offer which id is passed
+        /// </summary>
+        /// <remarks>
+        /// If no offer with given id is found, empty list is returned.
+        /// </remarks>
+        /// <param name="id">Id of job offer</param>
+        /// <returns>Job applications which are associated with job offer which id is passed</returns>
         [HttpGet("{id}")]
         public async Task<IEnumerable<JobApplication>> GetJobApplications(int id)
         {
             return await dataContext.JobApplications.Where(ja => ja.JobOfferId == id).ToListAsync();
         }
 
+        /// <summary>
+        /// Gets companies which name contains given string
+        /// </summary>
+        /// <remarks>
+        /// If searchString is null or empty, all companies are returned. 
+        /// </remarks>
+        /// <param name="searchString">Search string which will be used to filter companies</param>
+        /// <returns>Companies which name contains given string</returns>
         [HttpGet]
         [HttpGet("{searchString}")]
         public async Task<IEnumerable<Company>> GetCompanies(string searchString)
--- RecrutimentApp/Startup.cs
+++ RecrutimentApp/Startup.cs
@@ -1,17 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using RecrutimentApp.EntityFramework;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Http;
+using System.IO;
+using System.Reflection;
 using CommunityCertForT;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using RecrutimentApp.EntityFramework;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace RecrutimentApp
 {
@@ -49,11 +49,39 @@ namespace RecrutimentApp
             services.AddMvc();
             var connection = @"Server=(localdb)\mssqllocaldb;Database=RecruitmentAppDB;Trusted_Connection=True;";
             services.AddDbContext<DataContext>(options => options.UseSqlServer(connection));
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info
+                {
+                    Title = "RecrutimentApp",
+                    Version = "v1",
+                    Description = "WebApi documentation for RecruitmentApp",
+                    Contact = new Contact
+                    {
+                        Name = "Sebastian Safin",
+                        Email = "sebastian.safin@outlook.com"
+                    }
+                });
+                // Set the comments path for the Swagger JSON and UI.
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            //Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "RecrutimentApp API v1");
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
